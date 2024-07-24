1b8431c6539787e11d011bea625b82bc604efcae
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-07-24 23:23:58 +0530

    passing the input value to the useState hook and create addActivity fun add data to list

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './'
 
 export const ToDoList = () => {
+  
+    const [activity,setActivity] = useState("");
+    cont [listData,setListData] = useState([]);
+
+    function addActivity(){
+     setListData([...listData,activity]);
+     console.log(listData); 
+    }
+
   return (
     <div className='to_do_list'>
         <div className='container'>
             <h1>To Do List</h1>
           <div className='input'>
-            <input value= "" placeholder='Add Task' type='text' className='input'></input>
-            <button className='btn' >Add Task</button>
+            <input value= {activity} placeholder='Add Task' type='text' className='input' onChange={(e)=>{
+                setActivity(e.target.value);
+            }} ></input>
+            <button onClick={addActivity} className='btn' >Add Task</button>
           </div>
         </div>
     </div>
