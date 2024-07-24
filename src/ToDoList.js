6011398a7933f1f9ebac034e4e5900dc28e173f1
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-07-25 01:43:26 +0530

    we added css and complete our to-do-app

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,55 +1,59 @@
-import React, { useState } from 'react'
-import './'
+import React, { useState } from "react";
+import "./";
 
 export const ToDoList = () => {
-  
-    const [activity,setActivity] = useState("");
-    const [listData,setListData] = useState([]);
+  const [activity, setActivity] = useState("");
+  const [listData, setListData] = useState([]);
 
-    function addActivity(){
-     setListData((listData)=>{
-        const addedList = [...listData,activity];
-        console.log(addedList);
-        setActivity("");
-        return addedList;
-
-     })
-    }
-    
-  function removeActivity(i){
-    const filteringList = listData.filter((elem,id)=>{
-        return i != id;
-   
-    })
-    setListData(filteringList);
-
+  function addActivity() {
+    setListData((listData) => {
+      const addedList = [...listData, activity];
+      console.log(addedList);
+      setActivity("");
+      return addedList;
+    });
   }
 
+  function removeActivity(i) {
+    const filteringList = listData.filter((elem, id) => {
+      return i != id;
+    });
+    setListData(filteringList);
+  }
 
   return (
-    <div className='to_do_list'>
-        <div className='container'>
-            <h1>To Do List</h1>
-          <div className='input'>
-            <input value= {activity} placeholder='Add Task' type='text' className='input' onChange={(e)=>{
+    <div className="to_do_list">
+      <div className="container">
+        <h1>To Do List</h1>
+        <div className="input">
+          <div className="inputStyle">
+            <input
+              value={activity}
+              placeholder="Enter Task"
+              type="text"
+              className="input"
+              onChange={(e) => {
                 setActivity(e.target.value);
-            }} ></input>
-            <button onClick={addActivity} className='btn' >Add Task</button>
-            
-                {listData != [] && listData.map((data,i)=>{
-                    return(
-                        <ul className='ul' key={i}>
-                           <li className='li'>{data}<button onClick={()=>removeActivity(i)}>Remove</button></li>
-                        </ul>
-                      
-                
-                    )
-                })}
-            
-            
-
+              }}
+            ></input>
+            <button onClick={addActivity} className="btn">
+              Add Task
+            </button>
           </div>
+
+          {listData != [] &&
+            listData.map((data, i) => {
+              return (
+                <ul className="ul" key={i}>
+                  <li className="li">
+                    {data}
+                    <button className="rmBtn" onClick={() => removeActivity(i)}>Remove</button>
+                  </li>
+                </ul>
+              );
+            })}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
