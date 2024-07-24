af4b57a4e01d080f41214dd87a3256b8ad49ae64
Mangesh Jadhav <mangeshjadhavmrj@gmail.com>
2024-07-24 23:10:01 +0530

    create html structure in ToDoList.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import './'
 
 export const ToDoList = () => {
   return (
-    <div className='to_do_list'>ToDoList</div>
+    <div className='to_do_list'>
+        <div className='container'>
+            <h1>To Do List</h1>
+          <div className='input'>
+            <input value= "" placeholder='Add Task' type='text' className='input'></input>
+            <button className='btn' >Add Task</button>
+          </div>
+        </div>
+    </div>
   )
 }
